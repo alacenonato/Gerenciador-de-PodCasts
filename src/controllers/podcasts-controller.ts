5527d3b8d7720de5cec30a72c9c364470b2224c7
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-21 15:56:10 -0300

    Refatorando Camadas

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,10 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http';
-
-import {serviceListEpisodes} from '../services/list-episodes-service'
+import { serviceListEpisodes } from '../services/list-episodes-service'
 import { serviceFilterEpisodes } from '../services/filter-episodes-service';
 
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
-    
+
     const content = await serviceListEpisodes();
 
     response.writeHead(200, { 'content-type': "application/json" });
@@ -12,14 +11,17 @@ export const getListEpisodes = async (request: IncomingMessage, response: Server
 
 };
 
-export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
+export const getFilterEpisodes = async (
+    request: IncomingMessage,
+    response: ServerResponse
+) => {
 
     // http://localhost:3636/api/episode?p=flow
-    const queryString = request.url?.split("?p=")[1] || "";
 
-    const content = await serviceFilterEpisodes(queryString);
 
-    response.writeHead(200, {'content-type': "application/json"});
+    const content = await serviceFilterEpisodes(request.url);
+
+    response.writeHead(200, { 'content-type': "application/json" });
     response.end(JSON.stringify(content));
-    
+
 }
