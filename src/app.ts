1660db3cdeab9fc25f6a034bb84882dc488ed7db
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-21 23:00:17 -0300

    Responsabilidade Única

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+
+import * as http from 'http';
+import { getFilterEpisodes, getListEpisodes } from './controllers/podcasts-controller';
+import { Routes } from './routes/routes';
+import { HttpMethod } from './utils/http-methods';
+
+
+export const app = async (request: http.IncomingMessage, response: http.ServerResponse) => {
+    // query string
+    // http://localhost:3636/api/episode?p=flow
+
+    const [baseUrl, queryString] = request.url?.split('?') ?? ["", ""];
+    // console.log(baseUrl);
+    // console.log(queryString);
+
+    // listar podcasts
+    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
+        await getListEpisodes(request, response);
+    }
+
+    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
+        await getFilterEpisodes(request, response);
+    }
+
+};
+
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,8 @@
 
 import * as http from 'http';
+import { app } from './app';
 
-import { getFilterEpisodes, getListEpisodes } from './controllers/podcasts-controller';
-import { Routes } from './routes/routes';
-import { HttpMethod } from './utils/http-methods';
-
-const server = http.createServer(async (request: http.IncomingMessage, response: http.ServerResponse) => {
-    // query string
-    // http://localhost:3636/api/episode?p=flow
-
-    const [baseUrl, queryString] = request.url?.split('?') ?? ["", ""];
-    // console.log(baseUrl);
-    // console.log(queryString);
-
-    // listar podcasts
-    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
-        await getListEpisodes(request, response);
-    }
-
-    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
-        await getFilterEpisodes(request, response);
-    }
-
-
-});
+const server = http.createServer(app);
 
 const port = process.env.PORT
 server.listen(port, () => {
